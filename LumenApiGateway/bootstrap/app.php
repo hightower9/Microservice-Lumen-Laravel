1c2d268ede6f740ee8fecd44ede8a07ddbcd68d1
alister <alister.messi@gmail.com>
2020-11-01 21:19:17 +0000

    Added Lumen Generator in Gateway

--- LumenApiGateway/bootstrap/app.php
+++ LumenApiGateway/bootstrap/app.php
@@ -102,6 +102,7 @@ $app->routeMiddleware([
 $app->register(App\Providers\AuthServiceProvider::class);
 $app->register(Laravel\Passport\PassportServiceProvider::class);
 $app->register(Dusterio\LumenPassport\PassportServiceProvider::class);
+$app->register(Flipbox\LumenGenerator\LumenGeneratorServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
 
 /*
